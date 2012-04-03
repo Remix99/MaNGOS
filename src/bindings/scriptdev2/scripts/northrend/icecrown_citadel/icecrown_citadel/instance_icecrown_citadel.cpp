f7c5e4fbd9dc2305c79453e5a0f0f094dec96e62
Remix99 <remixcata@live.com>
2012-04-02 22:22:04 -0500

    [sd2_mr0381] Fixed door Ice Wall(Sindragosa)

--- src/bindings/scriptdev2/scripts/northrend/icecrown_citadel/icecrown_citadel/instance_icecrown_citadel.cpp
+++ src/bindings/scriptdev2/scripts/northrend/icecrown_citadel/icecrown_citadel/instance_icecrown_citadel.cpp
@@ -1,26 +1,26 @@
 /* Copyright (C) 2006 - 2012 ScriptDev2 <http://www.scriptdev2.com/>
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-*/
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 /* ScriptData
 SDName: instance_icecrown_spire
 SD%Complete: 90%
-SDComment: by michalpolko with special thanks to:
-mangosR2 team and all who are supporting us with feedback, testing and fixes
-TrinityCore for some info about spells IDs
-everybody whom I forgot to mention here ;)
+SDComment:  by michalpolko with special thanks to:
+            mangosR2 team and all who are supporting us with feedback, testing and fixes
+            TrinityCore for some info about spells IDs
+            everybody whom I forgot to mention here ;)
 
 SDCategory: Icecrown Citadel
 EndScriptData */
@@ -31,8 +31,8 @@ EndScriptData */
 
 static Locations SpawnLoc[]=
 {
-    {-446.788971f, 2003.362915f, 191.233948f}, // 0 Horde ship enter
-    {-428.140503f, 2421.336914f, 191.233078f}, // 1 Alliance ship enter
+    {-446.788971f, 2003.362915f, 191.233948f},  // 0 Horde ship enter
+    {-428.140503f, 2421.336914f, 191.233078f},  // 1 Alliance ship enter
 };
 
 instance_icecrown_spire::instance_icecrown_spire(Map* pMap) : ScriptedInstance(pMap)
@@ -248,6 +248,7 @@ void instance_icecrown_spire::OnObjectCreate(GameObject* pGo)
         case GO_GREEN_DRAGON_DOOR_1:
         case GO_BLOODWING_DOOR:
         case GO_ORATORY_DOOR:
+	case GO_SINDRAGOSA_ICE_WALL:
             m_mGoEntryGuidStore[pGo->GetEntry()] = pGo->GetObjectGuid();
             break;
     }
@@ -433,6 +434,7 @@ void instance_icecrown_spire::SetData(uint32 uiType, uint32 uiData)
             m_auiEncounter[TYPE_SINDRAGOSA] = uiData;
 
             DoUseDoorOrButton(GO_SINDRAGOSA_ENTRANCE);
+	    DoUseDoorOrButton(GO_SINDRAGOSA_ICE_WALL);
 
             if (uiData == DONE)
             {
